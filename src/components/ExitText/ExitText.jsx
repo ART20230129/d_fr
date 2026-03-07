1b547e8ced402b4236516071b731ed6c68ab4ff8
ART <tow1979@gmail.com>
2026-03-07 15:47:34 +0300

    sec comm

--- src/components/ExitText/ExitText.jsx
+++ src/components/ExitText/ExitText.jsx
@@ -1,15 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import S from './ExitText.module.css';
 
 export const ExitText = () => {
+  const navigate = useNavigate()
+
+  const handleSubmit = ()=>{
+    console.log('###!!!');
+    
+    const options = {
+      method: 'POST',
+      headers: { 'Authorization': `Token ${localStorage.getItem('token')}` },
+    };
+
+		try {
+			fetch('http://localhost:8000/logout/', options)
+        .then((response) => {
+          if (response.status == 200){
+            navigate('/', {replace: true}) //если пользователь нажимает кнопку «Назад», 
+                                          // он не сможет перейти на предыдущую страницу
+          }          
+        })
+
+		} catch (error) {
+			console.log('error: ', );			
+		}
+	}
   
   return (
     <nav className={S.navbar__nav}>
 
         <div className={S.nav__item}>
-          <Link className={S.nav__link} to="/">
+          <div className={S.nav__link} onClick={handleSubmit}>
             Выход
-          </Link>
+          </div>
         </div>
 
 
